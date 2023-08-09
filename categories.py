ffe9fb7f228860cfe47294c6d89ab44481c8d961
Костенков Даниил Дмитриевич <78727203+shlyapp@users.noreply.github.com>
2023-08-09 10:50:24 +0400

    add categories

---
+++ categories.py
@@ -0,0 +1,54 @@
+from typing import Dict, List, NamedTuple
+
+import db
+
+
+class Category(NamedTuple):
+    codename: str
+    name: str
+    is_base_expense: bool
+    aliases: List[str]
+
+
+class Categories:
+    def __init__(self):
+        self._categories = self._load_categories()
+
+    def _load_categories(self) -> List[Category]:
+        categories = db.fetchall(
+            "category", "codename name is_base_expense aliases".split()    
+        )
+        categories = self._fill_aliases(categories)
+        return categories
+
+    def _fill_aliases(self, categories: List[Dict]) -> List[Category]:
+        categories_result = []
+        for index, category in enumerate(categories):
+            aliases = category["aliases"].split(",")
+            aliases = list(filter(None, map(str.strip, aliases)))
+            aliases.append(category["codename"])
+            aliases.append(category["name"])
+            categories_result.append(Category(
+                codename=category['codename'],
+                name=category['name'],
+                is_base_expense=category['is_base_expense'],
+                aliases=aliases
+            ))
+        return categories_result   
+
+    def get_all_categories(self) -> List[Dict]:
+        return self._categories
+
+    def get_category(self, category_name: str) -> Category:
+        finded = None
+        other_category = None
+        for category in self._categories:
+            if category.codename == "other":
+                ohter_category = category
+            for alias in category.aliases:
+                if category_name in alias:
+                    finded = category
+        if not finded:
+            finded = other_category
+        return finded
+
