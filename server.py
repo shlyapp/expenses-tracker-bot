6a7d7402bac714edaec3d83a0828c1f87eee94d5
Костенков Даниил Дмитриевич <78727203+shlyapp@users.noreply.github.com>
2023-08-09 11:06:34 +0400

    ready to use bot

--- server.py
+++ server.py
@@ -2,6 +2,9 @@ import os
 
 from aiogram import Bot, Dispatcher, executor, types
 
+import exceptions
+import expenses
+from categories import Categories
 from middlewares import AccessMiddleware
 
 
@@ -15,7 +18,63 @@ dp.middleware.setup(AccessMiddleware(TELEGRAM_ACCESS_ID))
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welocome(message: types.Message):
-    await message.answer(f'Hello, {message.from_user.id}')
+    await message.answer(f'Привет! Это бот для учета финансов')
+
+
+@dp.message_handler(lambda message: message.text.startswith('/del'))
+async def delete_expense(message: types.Message):
+    row_id = int(message.text[4:])
+    expenses.delete_expense(row_id)
+    await message.answer("Удалил")
+
+
+@dp.message_handler(commands=['categories'])
+async def categories_list(message: types.Message):
+    categories = Categories().get_all_categories()
+    answer_message = "Категории трат:\n\n" +\
+        ("\n* ".join([c.name+' ('+", ".join(c.aliases)+')' for c in categories]))
+    await message.answer(answer_message)
+
+
+@dp.message_handler(commands=['today'])
+async def today_statictics(message: types.Message):
+    answer_message = expenses.get_today_statistics()
+    await message.answer(answer_message)
+
+
+@dp.message_handler(commands=['month'])
+async def month_statictics(message: types.Message):
+    answer_message = expenses.get_month_statistics()
+    await message.answer(answer_message)
+
+
+@dp.message_handler(commands=['expenses'])
+async def list_expenses(message: types.Message):
+    last_expenses = expenses.last()
+    if not last_expenses:
+        await message.answer("Расходы еще не заведены")
+        return
+
+    last_expenses_rows = [
+        f"{expense.amount} руб. на {expense.category_name} - нажми "
+        f"/del{expense.id} для удаления"
+        for expense in last_expenses]
+    answer_message = "Последние сохраненные траты:\n\n* " + "\n\n* "\
+            .join(last_expenses_rows)
+    await message.answer(answer_message)
+
+
+@dp.message_handler()
+async def add_expense(message: types.Message):
+    try:
+        expense = expenses.add_expense(message.text)
+    except exceptions.NotCorrectMessage as e:
+        await message.answer(str(e))
+        return
+    answer_message = (
+        f"Добавлены траты {expense.amount} руб. на {expense.category_name}.\n\n"
+        f"{expenses.get_today_statistics()}")
+    await message.answer(answer_message)
 
 
 if __name__ == '__main__':
